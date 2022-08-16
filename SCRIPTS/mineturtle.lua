390f21c5cb71cb5da8a5d7ee46bb362a1ac5e427
Кот <93428046+KoteTop8414@users.noreply.github.com>
2022-08-16 13:25:20 +0300

    Update mineturtle.lua

--- SCRIPTS/mineturtle.lua
+++ SCRIPTS/mineturtle.lua
@@ -1,7 +1,9 @@
 turtle.refuel()
 while true do
-turtle.dig()
-turtle.digUp()
-turtle.digDown()
-turtle.forward()
-end
+    turtle.dig()
+    turtle.digUp()
+    turtle.digDown()
+    turtle.forward()
+    gfl = turtle.GetFuelLevel()
+    print(gfl)
+exit
