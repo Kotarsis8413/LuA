f9b9a46e55616fe3fcead1a1386d0e514ed1c514
Кот <93428046+KoteTop8414@users.noreply.github.com>
2022-10-06 20:12:20 +0300

    Update script.lua

--- SCRIPTS/script.lua
+++ SCRIPTS/script.lua
@@ -1,1 +1,53 @@
-print("gg")
+local function trilaterate(A, B, C)
+    local a2b = B.vPosition - A.vPosition
+    local a2c = C.vPosition - A.vPosition
+
+    if math.abs(a2b:normalize():dot(a2c:normalize())) > 0.999 then
+        return nil
+    end
+
+    local d = a2b:length()
+    local ex = a2b:normalize( )
+    local i = ex:dot(a2c)
+    local ey = (a2c - ex * i):normalize()
+    local j = ey:dot(a2c)
+    local ez = ex:cross(ey)
+
+    local r1 = A.nDistance
+    local r2 = B.nDistance
+    local r3 = C.nDistance
+
+    local x = (r1 * r1 - r2 * r2 + d * d) / (2 * d)
+    local y = (r1 * r1 - r3 * r3 - x * x + (x - i) * (x - i) + j * j) / (2 * j)
+
+    local result = A.vPosition + ex * x + ey * y
+
+    local zSquared = r1 * r1 - x * x - y * y
+    if zSquared > 0 then
+        local z = math.sqrt(zSquared)
+        local result1 = result + ez * z
+        local result2 = result - ez * z
+
+        local rounded1, rounded2 = result1:round(0.01), result2:round(0.01)
+        if rounded1.x ~= rounded2.x or rounded1.y ~= rounded2.y or rounded1.z ~= rounded2.z then
+            return rounded1, rounded2
+        else
+            return rounded1
+        end
+    end
+    return result:round(0.01)
+
+end
+
+local function narrow(p1, p2, fix)
+    local dist1 = math.abs((p1 - fix.vPosition):length() - fix.nDistance)
+    local dist2 = math.abs((p2 - fix.vPosition):length() - fix.nDistance)
+
+    if math.abs(dist1 - dist2) < 0.01 then
+        return p1, p2
+    elseif dist1 < dist2 then
+        return p1:round(0.01)
+    else
+        return p2:round(0.01)
+    end
+end
